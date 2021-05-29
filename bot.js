07ea83ef608fe251cfed41a8f405273191edbd79
krishpranav <krisna.pranav@gmail.com>
2021-05-29 11:08:20 +0530

    [+] update msg List [+]

--- bot.js
+++ bot.js
@@ -10,4 +10,28 @@
 		"chat_active": [0, 0],
 		"selected_title": [0, 0, 5, 3, 0, 1, 1, 0, 0, 0, 0]
 	};
+
+    const msgList = [
+		`
+		Husband and Wife had a Fight.
+		Wife called Mom : He fought with me again,
+		I am coming to you.
+		Mom : No beta, he must pay for his mistake,
+		I am comming to stay with U!`,
+
+		`
+		Husband: Darling, years ago u had a figure like Coke bottle.
+		Wife: Yes darling I still do, only difference is earlier it was 300ml now it's 1.5 ltr.`,
+
+		`
+		God created the earth, 
+		God created the woods, 
+		God created you too, 
+		But then, even God makes mistakes sometimes!`,
+
+		`
+		What is a difference between a Kiss, a Car and a Monkey? 
+		A kiss is so dear, a car is too dear and a monkey is U dear.`
+	]
+
 })
