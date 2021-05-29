3090ca158713539cc32353379ecd92b3fea250b9
krishpranav <krisna.pranav@gmail.com>
2021-05-29 11:24:59 +0530

    [+] update bot [+]

--- bot.js
+++ bot.js
@@ -1,8 +1,7 @@
 (() => {
-
-    var lastMessageOnChat = false;
-    var ignoreLastMsg = {};
-    var elementConfig = {
+	var lastMessageOnChat = false;
+	var ignoreLastMsg = {};
+	var elementConfig = {
 		"chats": [0, 0, 5, 2, 0, 3, 0, 0, 0],
 		"chat_icons": [0, 0, 1, 1, 1, 0],
 		"chat_title": [0, 0, 1, 0, 0, 0, 0],
@@ -11,7 +10,7 @@
 		"selected_title": [0, 0, 5, 3, 0, 1, 1, 0, 0, 0, 0]
 	};
 
-    const msgList = [
+	const msgList = [
 		`
 		Husband and Wife had a Fight.
 		Wife called Mom : He fought with me again,
@@ -34,10 +33,11 @@
 		A kiss is so dear, a car is too dear and a monkey is U dear.`
 	]
 
-    function rand(high, low = 0) {
-        return Math.floor(Math.random() * (high - low + 1) + low);
-    }
 
+	function rand(high, low = 0) {
+		return Math.floor(Math.random() * (high - low + 1) + low);
+	}
+	
 	function getElement(id, parent){
 		if (!elementConfig[id]){
 			return false;
@@ -52,7 +52,7 @@
 		});
 		return elem;
 	}
-
+	
 	function getLastMsg(){
 		var messages = document.querySelectorAll('.msg');
 		var pos = messages.length-1;
@@ -69,6 +69,179 @@
 			return false;
 		}
 	}
+	
+	function getUnreadChats(){
+		var unreadchats = [];
+		var chats = getElement("chats");
+		if (chats){
+			chats = chats.childNodes;
+			for (var i in chats){
+				if (!(chats[i] instanceof Element)){
+					continue;
+				}
+				var icons = getElement("chat_icons", chats[i]).childNodes;
+				if (!icons){
+					continue;
+				}
+				for (var j in icons){
+					if (icons[j] instanceof Element){
+						if (!(icons[j].childNodes[0].getAttribute('data-icon') == 'muted' || icons[j].childNodes[0].getAttribute('data-icon') == 'pinned')){
+							unreadchats.push(chats[i]);
+							break;
+						}
+					}
+				}
+			}
+		}
+		return unreadchats;
+	}
+	
+	function didYouSendLastMsg(){
+		var messages = document.querySelectorAll('.msg');
+		if (messages.length <= 0){
+			return false;
+		}
+		var pos = messages.length-1;
+		
+		while (messages[pos] && messages[pos].classList.contains('msg-system')){
+			pos--;
+			if (pos <= -1){
+				return -1;
+			}
+		}
+		if (messages[pos].querySelector('.message-out')){
+			return true;
+		}
+		return false;
+	}
+
+	const goAgain = (fn, sec) => {
+		setTimeout(fn, sec * 1000)
+	}
+
+	const eventFire = (el, etype) => {
+		var evt = document.createEvent("MouseEvents");
+		evt.initMouseEvent(etype, true, true, window,0, 0, 0, 0, 0, false, false, false, false, 0, null);
+		el.dispatchEvent(evt);
+	}
+
+	const selectChat = (chat, cb) => {
+		const title = getElement("chat_title",chat).title;
+		eventFire(chat.firstChild.firstChild, 'mousedown');
+		if (!cb) return;
+		const loopFewTimes = () => {
+			setTimeout(() => {
+				const titleMain = getElement("selected_title").title;
+				if (titleMain !== undefined && titleMain != title){
+					console.log('not yet');
+					return loopFewTimes();
+				}
+				return cb();
+			}, 300);
+		}
+
+		loopFewTimes();
+	}
+
+	const sendMessage = (chat, message, cb) => {
+		var title;
+
+		if (chat){
+			title = getElement("chat_title",chat).title;
+		} else {
+			title = getElement("selected_title").title;
+		}
+		ignoreLastMsg[title] = message;
+		
+		messageBox = document.querySelectorAll("[contenteditable='true']")[1];
 
 
-})
+		messageBox.innerHTML = message.replace(/  /gm,'');
+
+		event = document.createEvent("UIEvents");
+		event.initUIEvent("input", true, true, window, 1);
+		messageBox.dispatchEvent(event);
+
+		eventFire(document.querySelector('span[data-icon="send"]'), 'click');
+
+		cb();
+	}
+
+	const start = (_chats, cnt = 0) => {
+		const chats = _chats || getUnreadChats();
+		const chat = chats[cnt];
+		
+		var processLastMsgOnChat = false;
+		var lastMsg;
+		
+		if (!lastMessageOnChat){
+			if (false === (lastMessageOnChat = getLastMsg())){
+				lastMessageOnChat = true; 
+			} else {
+				lastMsg = lastMessageOnChat;
+			}
+		} else if (lastMessageOnChat != getLastMsg() && getLastMsg() !== false && !didYouSendLastMsg()){
+			lastMessageOnChat = lastMsg = getLastMsg();
+			processLastMsgOnChat = true;
+		}
+		
+		if (!processLastMsgOnChat && (chats.length == 0 || !chat)) {
+			console.log(new Date(), 'nothing to do now... (1)', chats.length, chat);
+			return goAgain(start, 3);
+		}
+
+		var title;
+		if (!processLastMsgOnChat){
+			title = getElement("chat_title",chat).title + '';
+			lastMsg = (getElement("chat_lastmsg", chat) || { innerText: '' }).title.replace(/[\u2000-\u206F]/g, "");
+		} else {
+			title = getElement("selected_title").title;
+		}
+		if (ignoreLastMsg[title] && (ignoreLastMsg[title]) == lastMsg) {
+			console.log(new Date(), 'nothing to do now... (2)', title, lastMsg);
+			return goAgain(() => { start(chats, cnt + 1) }, 0.1);
+		}
+
+		let sendText
+
+		if (lastMsg.toUpperCase().indexOf('@HELP') > -1){
+			sendText = `
+				Cool ${title}! Some commands that you can send me:
+
+				1. *@TIME*
+				2. *@JOKE*`
+		}
+
+		if (lastMsg.toUpperCase().indexOf('@TIME') > -1){
+			sendText = `
+				Don't you have a clock, dude?
+
+				*${new Date()}*`
+		}
+
+		if (lastMsg.toUpperCase().indexOf('@JOKE') > -1){
+			sendText = msgList[rand(msgList.length - 1)];
+		}
+		
+		if (!sendText) {
+			ignoreLastMsg[title] = lastMsg;
+			console.log(new Date(), 'new message ignored -> ', title, lastMsg);
+			return goAgain(() => { start(chats, cnt + 1) }, 0.1);
+		}
+
+		console.log(new Date(), 'new message to process, uhull -> ', title, lastMsg);
+
+		if (!processLastMsgOnChat){
+			selectChat(chat, () => {
+				sendMessage(chat, sendText.trim(), () => {
+					goAgain(() => { start(chats, cnt + 1) }, 1);
+				});
+			})
+		} else {
+			sendMessage(null, sendText.trim(), () => {
+				goAgain(() => { start(chats, cnt + 1) }, 1);
+			});
+		}
+	}
+	start();
+})()
