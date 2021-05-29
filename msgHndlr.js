2f936f5b51746655aeb23686b563d48e646ce17e
krishpranav <krisna.pranav@gmail.com>
2021-05-29 10:34:01 +0530

    [+] added const values in try block [+]

--- msgHndlr.js
+++ msgHndlr.js
@@ -15,3 +15,20 @@ const { stdout } = require('process')
 const nsfw_ = JSON.parse(fs.readFileSync('./lib/NSFW.json'))
 const welkom = JSON.parse(fs.readFileSync('./lib/welcome.json'))
 const { RemoveBgResult, removeBackgroundFromImageBase64, removeBackgroundFromImageFile } = require('remove.bg')
+
+moment.tz.setDefault('Asia/Jakarta').locale('id')
+
+module.exports = msgHandler = async(client, message) => {
+    try {
+        const { type, id, from, t, sender, isGroupMsg, chat, caption, isMedia, mimetype, quotedMsg, quotedMsgObj, mentionedJidList } = message
+        let { body } = message
+        const { name, formattedTitle } = chat
+        let { pushname, verifiedName } = sender
+        pushname = pushname || verifiedName
+        const commands = caption || body || ''
+        const command = commands.toLowerCase().split(' ')[0] || ''
+        const args = commands.split(' ')
+
+
+    }
+}
