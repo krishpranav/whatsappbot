70e19b5649d05b8ebfc52e38e0fc2eab36bfa254
krishpranav <krisna.pranav@gmail.com>
2021-05-29 10:23:55 +0530

    [+] onStateChanged functions [+]

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ const { create, Client } = require('@open-wa/wa-automate')
 const welcome = require('./lib/welcome')
 const msgHandler = require('./msgHndlr')
 const options = require('./options')
+
+
+const start = async(client = new Client()) => {
+    console.log('[SERVER] Server Started!!!....')
+    client.onStateChanged((state) => {
+        console.log('[CLIENT STATE]', state)
+        if (state === 'CONFLICT' || state === 'UNLAUNCHED') client.forceRefocus()
+    })
+}
