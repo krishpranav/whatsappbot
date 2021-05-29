6e16fab27d1bbbfa40bdef8c43f90d9e3185b47d
krishpranav <krisna.pranav@gmail.com>
2021-05-29 11:05:02 +0530

    [+] Added variables [+]

---
+++ bot.js
@@ -0,0 +1,13 @@
+(() => {
+
+    var lastMessageOnChat = false;
+    var ignoreLastMsg = {};
+    var elementConfig = {
+		"chats": [0, 0, 5, 2, 0, 3, 0, 0, 0],
+		"chat_icons": [0, 0, 1, 1, 1, 0],
+		"chat_title": [0, 0, 1, 0, 0, 0, 0],
+		"chat_lastmsg": [0, 0, 1, 1, 0, 0],
+		"chat_active": [0, 0],
+		"selected_title": [0, 0, 5, 3, 0, 1, 1, 0, 0, 0, 0]
+	};
+})
